044a59c274831101003ba6c5ba9d83537586106b
Gregory Fournier <gregorykarlfournier@gmail.com>
2022-12-04 06:15:19 -0500

    day4

---
+++ src/main/kotlin/day04.kt
@@ -0,0 +1,36 @@
+fun main(args: Array<String>) {
+    Day04.partTwo()
+}
+
+object Day04 {
+
+    fun partOne() {
+        val s = readLines("4")
+            .map {
+                it.split(",").map {
+                    val range = it.split("-")
+                    (range[0].toInt()..range[1].toInt()).toSet()
+                }
+            }
+            .filter { pairs ->
+                val interesct = pairs.first().intersect(pairs.last())
+                interesct == pairs.first() || interesct == pairs.last()
+            }.size
+        println(s)
+    }
+
+    fun partTwo() {
+        val s = readLines("4")
+            .map {
+                it.split(",").map {
+                    val range = it.split("-")
+                    (range[0].toInt()..range[1].toInt()).toSet()
+                }
+            }
+            .filter { pairs ->
+                val interesct = pairs.first().intersect(pairs.last())
+                interesct.isNotEmpty()
+            }.size
+        println(s)
+    }
+}
